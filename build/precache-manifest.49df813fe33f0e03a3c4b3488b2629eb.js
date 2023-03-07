5445929c3824f168ba4532e9cad681c440ded20f
KulakIlya <kulak1224@gmail.com>
2023-03-07 11:27:50 +0200

    +

---
+++ build/precache-manifest.49df813fe33f0e03a3c4b3488b2629eb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6b60e8497d304f6ba8b15e26a25b29",
+    "url": "/keeperapp/index.html"
+  },
+  {
+    "revision": "efafa8cab2d9fe152038",
+    "url": "/keeperapp/static/js/2.16de64e5.chunk.js"
+  },
+  {
+    "revision": "dfbe67cdd8b3f8ac7d4b",
+    "url": "/keeperapp/static/js/main.34149b2e.chunk.js"
+  },
+  {
+    "revision": "e4fabe411e690a895a41",
+    "url": "/keeperapp/static/js/runtime-main.f498bfa9.js"
+  }
+]);
